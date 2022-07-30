5b97b39ca4e722bfb8532d0c0963438bbd3149ed
Omar Estrella <oyestrella91@gmail.com>
2022-07-29 21:35:20 -0400

    Initial Commit

---
+++ Shared/JanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  JanetApp.swift
+//  Shared
+//
+//  Created by Omar Estrella on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct JanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
